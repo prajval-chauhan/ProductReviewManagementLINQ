2b0e6f76042588275eaa2f1c6a939c8bb2834fd4
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-11-18 22:58:01 +0530

    [Prajval]Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductReviewManagementLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the product review management program");
+        }
+    }
+}
